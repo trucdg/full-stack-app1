3394f5cb56b17e6b7d025920bd623b7c3d04c91f
Truc Duong <trucdg@bu.edu>
2023-09-09 00:51:56 -0400

    declare abstract method getAllStudents() in the StudentService interface

--- src/main/java/com/trucdg/studentsystem/service/StudentService.java
+++ src/main/java/com/trucdg/studentsystem/service/StudentService.java
@@ -3,6 +3,10 @@ package com.trucdg.studentsystem.service;
 import com.trucdg.studentsystem.model.Student;
 import jakarta.persistence.Entity;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
+
 }
