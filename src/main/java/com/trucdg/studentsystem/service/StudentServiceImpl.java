18b36ac4e7d8a425cead0a610cbf4bb8125cd522
Truc Duong <trucdg@bu.edu>
2023-09-09 00:52:54 -0400

    add method getAllStudents() implementation to StudentServiceImpl class

--- src/main/java/com/trucdg/studentsystem/service/StudentServiceImpl.java
+++ src/main/java/com/trucdg/studentsystem/service/StudentServiceImpl.java
@@ -5,6 +5,8 @@ import com.trucdg.studentsystem.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class StudentServiceImpl implements StudentService {
 
@@ -17,5 +19,8 @@ public class StudentServiceImpl implements StudentService {
         return studentRepository.save(student);
     }
 
-
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
 }
