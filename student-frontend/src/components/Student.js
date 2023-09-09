a32ffaf660a6c4fb777d2cd5e96358b940c13b18
Truc Duong <trucdg@bu.edu>
2023-09-09 19:06:06 -0400

    fetch students information from database using get, and add students using post method

--- student-frontend/src/components/Student.js
+++ student-frontend/src/components/Student.js
@@ -1,15 +1,43 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
+import { useEffect, useState } from "react";
 import TextField from "@mui/material/TextField";
 import { Container, Paper } from "@mui/material";
+import Button from "@mui/material/Button";
 
 export default function Student() {
   const paperStyle = { padding: "50px 20px", width: 600, margin: "20px auto" };
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [students, setStudents] = useState([]);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const student = { name, address };
+    console.log(student);
+    fetch("http://localhost:8080/student/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(student),
+    }).then(() => {
+      console.log("New student added!");
+    });
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:8080/student/getAll")
+      .then((res) => res.json())
+      .then((result) => {
+        setStudents(result);
+      });
+  }, []);
+
   return (
     <Container>
       <Paper elevation={3} style={paperStyle}>
-        <h1>Add Student</h1>
-        <Box
+        <h1 style={{ color: "blue" }}>
+          <u>Add Student</u>
+        </h1>
+        <form
           component="form"
           sx={{
             "& > :not(style)": { m: 1, width: "25ch" },
@@ -18,18 +46,41 @@ export default function Student() {
           autoComplete="off"
         >
           <TextField
+            style={{ padding: "10px" }}
             id="outlined-basic"
             label="Student Name"
             variant="outlined"
             fullWidth
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
+
           <TextField
+            style={{ padding: "10px" }}
             id="outlined-basic"
             label="Student Address"
             variant="outlined"
             fullWidth
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
           />
-        </Box>
+          <Button variant="contained" onClick={handleClick}>
+            Submit
+          </Button>
+        </form>
+      </Paper>
+      <Paper elevation={3} style={paperStyle}>
+        {students.map((student) => (
+          <Paper
+            elevation={6}
+            style={{ margin: "10px", padding: "15px", textAlign: "left" }}
+            key={student.id}
+          >
+            Id:{student.id}
+            Name:{student.name}
+            Address: {student.address}
+          </Paper>
+        ))}
       </Paper>
     </Container>
   );
