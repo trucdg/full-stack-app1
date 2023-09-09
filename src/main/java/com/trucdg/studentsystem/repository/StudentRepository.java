55f677419cec239ccb16b33edf2abefd4d5e22dc
Truc Duong <trucdg@bu.edu>
2023-09-09 00:45:26 -0400

    initialize StudentRepository interface to extends JpaRepository

---
+++ src/main/java/com/trucdg/studentsystem/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.trucdg.studentsystem.repository;
+
+import com.trucdg.studentsystem.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
