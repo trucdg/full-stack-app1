09d0f6da7162b221c76689b3a2e4af8a5c49c1eb
Truc Duong <trucdg@bu.edu>
2023-09-09 00:47:25 -0400

    create StudentController class to map http methods

---
+++ src/main/java/com/trucdg/studentsystem/controller/StudentController.java
@@ -0,0 +1,24 @@
+package com.trucdg.studentsystem.controller;
+
+import com.trucdg.studentsystem.model.Student;
+import com.trucdg.studentsystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Student student) {
+        studentService.saveStudent(student);
+        return "New student is added";
+    }
+
+}
