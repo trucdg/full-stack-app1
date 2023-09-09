aafdcbfa8c21b783725a33680730921b587dd682
Truc Duong <trucdg@bu.edu>
2023-09-09 00:53:56 -0400

    add GetMapping to get all students

--- src/main/java/com/trucdg/studentsystem/controller/StudentController.java
+++ src/main/java/com/trucdg/studentsystem/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.trucdg.studentsystem.controller;
 import com.trucdg.studentsystem.model.Student;
 import com.trucdg.studentsystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -19,6 +18,11 @@ public class StudentController {
     public String add(@RequestBody Student student) {
         studentService.saveStudent(student);
         return "New student is added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
     }
 
 }
